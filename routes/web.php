addb64b26b8a2b58f226450227e851b6ca2ab1c9
Leunhart <jessicajeffry991@gmail.com>
2023-12-14 20:26:03 +0700

    versi 1

---
+++ routes/web.php
@@ -0,0 +1,17 @@
+//laravelproject\routes\web.php
+<?php
+ 
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CustomAuthController;
+ 
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+ 
+Route::get('/', [CustomAuthController::class, 'home']); 
+Route::get('dashboard', [CustomAuthController::class, 'dashboard']); 
+Route::get('login', [CustomAuthController::class, 'index'])->name('login');
+Route::post('postlogin', [CustomAuthController::class, 'login'])->name('postlogin'); 
+Route::get('signup', [CustomAuthController::class, 'signup'])->name('register-user');
+Route::post('postsignup', [CustomAuthController::class, 'signupsave'])->name('postsignup'); 
+Route::get('signout', [CustomAuthController::class, 'signOut'])->name('signout');
